c95e28919e4745ae681060ce2c076d8241476e8f
bablu <aniketpanchal07@gmail.com>
2024-03-25 22:54:37 +0530

    added backend files

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
@@ -10,7 +11,31 @@ const app = express();
 
 
 
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(3000, () => {
+  app.use('/api/user', userRoutes);
+  app.use('/api/auth', authRoutes);
+
+
+  
+  app.get("/", (req, res, next) => {
+    try {
+      res.status(200).json({
+        message: " api is live ",
+        creator: "Aniket panchal (me)"
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+app.listen(3000, (req,res,next) => {
     console.log('Server listening on port 3000');
   });
