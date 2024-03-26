2790fe002f50ccbeeaf42a012c1c29d958756fe7
bablu <aniketpanchal07@gmail.com>
2024-03-26 15:12:46 +0530

    added redux

--- client/src/pages/SignIn2.jsx
+++ client/src/pages/SignIn2.jsx
@@ -1,36 +1,24 @@
-import { useState, ChangeEvent, FormEvent } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 // redux
-import {
-  signInStart,
-  signInSuccess,
-  signInFailure,
-} from '../redux/user/UserSlice';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/UserSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
-export default function SignIn() {
-  const [formData, setFormData] = useState<{ [key: string]: string }>({});
-  // const [error, setError] = useState<boolean>(false);
-  // const [loading, setLoading] = useState<boolean>(false);  
+export default function SignIn2() {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-  const {loading,error}= useSelector((state)=> state.user);
-
-
-  const navigate = useNavigate();
-  const dispatch =useDispatch()
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = e => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async e => {
     e.preventDefault();
     try {
-      // setLoading(true);
-      // setError(false);
-      dispatch(signInStart())   // redux
-
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -39,23 +27,20 @@ export default function SignIn() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-    
+      dispatch(signInSuccess(data));
       if (data.success === false) {
-        dispatch(signInFailure(data.message))
+        dispatch(signInFailure(null));
         return;
       }
-        // setLoading(false);
-        dispatch(signInSuccess(data)); //redux
-      navigate('/');
+      history.push('/');
     } catch (error) {
-      // setLoading(false);
-      // setError(true);
       dispatch(signInFailure(error));
     }
   };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
+        hello
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
@@ -85,7 +70,7 @@ export default function SignIn() {
           <span className='text-blue-500'>Sign up</span>
         </Link>
       </div>
-      <p className='text-red-700 mt-5'>{error ? error || 'Something went wrong!': ""}</p>
+      <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
     </div>
   );
 }
---
+++ client/src/redux/store.ts
@@ -0,0 +1,13 @@
+import {configureStore} from "@reduxjs/toolkit";
+import  useReducer  from "./user/UserSlice";
+
+
+export const store= configureStore({
+    reducer:{user:useReducer},
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+})
+
+
