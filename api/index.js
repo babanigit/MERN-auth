c8e800859310b48d9338492d3f798d4cc0083f55
bablu <aniketpanchal07@gmail.com>
2024-03-27 13:45:18 +0530

    edited

--- api/index.js
+++ api/index.js
@@ -8,7 +8,8 @@ import path from 'path';
 dotenv.config();
 
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+// const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __dirname = path.resolve();
 
 
 const app = express();
