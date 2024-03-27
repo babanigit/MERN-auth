1dc5ece25671c93a5f2fabd44b17991774cb3ba2
bablu <aniketpanchal07@gmail.com>
2024-03-27 13:13:12 +0530

    added __dirname, 'client2'

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client2', 'dist', 'index.html'));
 });
 
 
